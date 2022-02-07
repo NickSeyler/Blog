7b2bbb5e44408b424aa1f125ec565c7ac07f0970
NickSeyler <nickseyler25@gmail.com>
2022-02-07 17:29:03 -0500

    Add project files.

---
+++ Blog/Models/BlogUser.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace Blog.Models
+{
+    public class BlogUser : IdentityUser
+    {
+        public string FirstName { get; set; } = "";
+        public string LastName { get; set; } = "";
+        public string? DisplayName { get; set; }
+    }
+}
