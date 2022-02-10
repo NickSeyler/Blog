7dcdf46e184b309ecd5c7dc753b1a3573112b888
NickSeyler <nickseyler25@gmail.com>
2022-02-10 12:32:41 -0500

    Updated database

---
+++ Blog/Data/Migrations/20220210173202_002.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Blog.Data.Migrations
+{
+    public partial class _002 : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<byte[]>(
+                name: "ImageData",
+                table: "BlogItems",
+                type: "bytea",
+                nullable: false,
+                defaultValue: new byte[0]);
+
+            migrationBuilder.AddColumn<string>(
+                name: "ImageType",
+                table: "BlogItems",
+                type: "text",
+                nullable: false,
+                defaultValue: "");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "ImageData",
+                table: "BlogItems");
+
+            migrationBuilder.DropColumn(
+                name: "ImageType",
+                table: "BlogItems");
+        }
+    }
+}
