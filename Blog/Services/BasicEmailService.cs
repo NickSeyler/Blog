86bcb4799546875499e38bb227206d8faedf6257
NickSeyler <nickseyler25@gmail.com>
2022-02-22 15:36:04 -0500

    Blog Layout slightly modified, setting up the BasicEmailService

---
+++ Blog/Services/BasicEmailService.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Identity.UI.Services;
+
+namespace Blog.Services
+{
+    public class BasicEmailService : IEmailSender
+    {
+        private readonly IConfiguration _appSettings;
+
+        public BasicEmailService(IConfiguration appSettings)
+        {
+            _appSettings = appSettings;
+        }
+
+        public Task SendEmailAsync(string email, string subject, string htmlMessage)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
