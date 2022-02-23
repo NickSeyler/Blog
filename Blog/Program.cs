86d753a498791c7b5ea32283e8025a2ebc22aff1
NickSeyler <nickseyler25@gmail.com>
2022-02-23 12:03:18 -0500

    Removed unnecessary login text, added lots of styled, improved blog child indexlooks

--- Blog/Program.cs
+++ Blog/Program.cs
@@ -7,7 +7,6 @@ using Microsoft.AspNetCore.Identity.UI.Services;
 using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
-
 //var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
 var connectionString = ConnectionService.GetConnectionString(builder.Configuration);
 
