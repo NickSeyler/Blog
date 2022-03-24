b393427256c4b092dbad751b25a3ccb687c3a2e0
NickSeyler <nickseyler25@gmail.com>
2022-03-24 10:38:22 -0400

    Undid removal of route

--- Blog/Program.cs
+++ Blog/Program.cs
@@ -102,6 +102,11 @@ app.UseSwaggerUI(s =>
     s.SwaggerEndpoint("/swagger/v1/swagger.json", "Blog API");
     s.InjectStylesheet("~/css/SwaggerUI.css");
     s.InjectJavascript("~/js/SwaggerUI.js");
+
+    if (!app.Environment.IsDevelopment())
+    {
+        s.RoutePrefix = "";
+    }
 });
 
 app.MapControllerRoute(
